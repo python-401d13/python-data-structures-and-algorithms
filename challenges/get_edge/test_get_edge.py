421613b08461d15c2bffb1cb6019342fd10e764c
osethan <GE47R9xmtouch .zshrcFASEPs@0>
2020-01-28 15:46:00 -0800

    Graph get_edge function code and tests written

---
+++ challenges/get_edge/test_get_edge.py
@@ -0,0 +1,83 @@
+from challenges.graph.graph import Graph
+from challenges.get_edge.get_edge import get_edge
+import pytest
+
+
+def test_graph_empty():
+    """
+    Trip through empty can't happen.
+    """
+
+    graph = Graph()
+    cities = ['a', 'b', 'c']
+
+    expected = 'False:$0'
+    actual = get_edge(graph, cities)
+    assert actual == expected
+
+
+def test_cities_empty(graph):
+    """
+    Trip with no cities means no trip.
+    """
+
+    expected = 'False:$0'
+    actual = get_edge(graph, [])
+    assert actual == expected
+
+
+def test_get_edge(graph):
+    """
+    Trip through graph can happen.
+    """
+
+    cities = ['a', 'b', 'c']
+
+    expected = 'True:$249'
+    actual = get_edge(graph, cities)
+    assert actual == expected
+
+
+# Fixtures
+
+
+@pytest.fixture
+def graph():
+    """
+    Graph instance.
+    """
+
+    graph = Graph()
+
+    a = graph.add_node('a')
+    b = graph.add_node('b')
+    c = graph.add_node('c')
+    d = graph.add_node('d')
+    e = graph.add_node('e')
+    f = graph.add_node('f')
+
+    graph.add_edge(a, b, 150)
+    graph.add_edge(a, c, 82)
+
+    graph.add_edge(b, a, 150)
+    graph.add_edge(b, c, 99)
+    graph.add_edge(b, d, 42)
+
+    graph.add_edge(c, a, 82)
+    graph.add_edge(c, b, 99)
+    graph.add_edge(c, d, 105)
+    graph.add_edge(c, e, 37)
+    graph.add_edge(c, f, 26)
+
+    graph.add_edge(d, b, 42)
+    graph.add_edge(d, c, 105)
+    graph.add_edge(d, f, 73)
+
+    graph.add_edge(e, c, 37)
+    graph.add_edge(e, f, 250)
+
+    graph.add_edge(f, c, 26)
+    graph.add_edge(f, d, 73)
+    graph.add_edge(f, e, 250)
+
+    return graph
